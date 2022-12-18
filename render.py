f98a25c79ba07336a731c2362d911e146182d390
ivanvolkov <volkov.ivan20032012@yandex.ru>
2022-12-18 21:59:04 +0300

    v3

--- render.py
+++ render.py
@@ -1,14 +1,8 @@
-from jinja2 import Template
-import os
+from jinja2 import Environment, FileSystemLoader
 
 
 def render(template_name, folder='templates', **kwargs):
-
-    file_path = os.path.join(folder, template_name)
-
-    with open(file_path, encoding='utf-8') as f:
-        template = Template(f.read())
-
-    return template.render(**kwargs)
-
-
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    tmpl = env.get_template(template_name)
+    return tmpl.render(**kwargs)
--- views.py
+++ views.py
@@ -1,3 +1,6 @@
+import datetime
+import os.path
+
 from render import render
 from data.services import services
 from data.users import users
@@ -13,3 +16,25 @@ def services_view(request):
 
 def users_view(request):
     return '200 OK', render('users.html', folder='templates', users=users)
+
+
+def contact_view(request):
+    if request['method'] == 'POST':
+        now = datetime.datetime.now()
+        data = request['data']
+        title = data['title']
+        text = data['text']
+        email = data['email']
+
+        if not os.path.exists('messages'):
+            os.mkdir('messages')
+
+        with open(f'messages/message_{now.strftime("%d%m%Y")}_{now.strftime("%H%M%S")}.txt', 'w',
+                      encoding='utf-8') as file:
+            file.write(f'Нам пришло сообщение от {now.strftime("%d.%m.%Y")} в {now.strftime("%H:%M:%S")}!\n'
+                           f'Отправитель: {email}\n'
+                           f'Тема: {title}\n'
+                           f'Текст: {text}')
+        return '200 OK', render('contacts.html')
+    else:
+        return '200 OK', render('contacts.html')
